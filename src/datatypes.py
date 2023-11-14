255a0e3351a84cc180a2b84a159db044a5ef5148
gildossantos <gilbertodossantos74@gmail.com>
2023-11-14 17:02:00 -0500

    fix init

--- src/datatypes.py
+++ src/datatypes.py
@@ -28,7 +28,7 @@ class FBAssociation(FBEntity):
     """A generic FlyBase association/annotation."""
     def __init__(self, chado_objs):
         """Create the generic FlyBase association/annotation object from the main db entry/entries."""
-        super().__init__(chado_objs)
+        super().__init__()
         self.chado_objs = chado_objs    # The defining SQLAlchemy chado object(s).
 
 
@@ -36,7 +36,7 @@ class FBDataEntity(FBEntity):
     """A generic FlyBase data entity with all it related data, excluding associations/annotations."""
     def __init__(self, chado_obj):
         """Create the generic FlyBase data entity object from the main db entry."""
-        super().__init__(chado_obj)
+        super().__init__()
         # Primary FB chado data.
         self.chado_obj = chado_obj      # The primary SQLAlchemy chado object.
         self.pubs = []                  # Pub associations: e.g., FeaturePub, StrainPub.
