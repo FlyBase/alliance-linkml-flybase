a8e6fa594134543e9a415a2c719784b6ebe65dd2
gildossantos <gilbertodossantos74@gmail.com>
2024-05-23 14:54:31 -0400

    typo

--- src/agm_handlers.py
+++ src/agm_handlers.py
@@ -98,7 +98,7 @@ class StrainHandler(PrimaryEntityHandler):
             agr_strain = agr_datatypes.AffectedGenomicModelDTO()
             agr_strain.obsolete = strain.chado_obj.is_obsolete
             agr_strain.mod_entity_id = f'FB:{strain.uniquename}'
-            agr_strain.mod_internal_id = str(agr_strain.chado_obj.strain_id)
+            agr_strain.mod_internal_id = str(strain.chado_obj.strain_id)
             agr_strain.taxon_curie = strain.ncbi_taxon_id
             agr_strain.name = strain.name
             agr_strain.subtype_name = 'strain'
