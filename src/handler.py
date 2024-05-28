87b3fdf8f65e5a4f035c371a3806836ad5ad5b69
gildossantos <gilbertodossantos74@gmail.com>
2024-05-28 11:37:00 -0400

    adjust timestamp mapping to account for initial FB objects with no Alliance export mapping

--- src/handler.py
+++ src/handler.py
@@ -614,7 +614,7 @@ class DataHandler(object):
         """Map timestamps to Alliance object."""
         self.log.info('Map timestamps to Alliance object.')
         for fb_data_entity in self.fb_data_entities.values():
-            if fb_data_entity.timestamps:
+            if fb_data_entity.timestamps and fb_data_entity.linkmldto is not None:
                 fb_data_entity.linkmldto.date_created = strict_rfc3339.\
                     timestamp_to_rfc3339_localoffset(datetime.datetime.timestamp(min(fb_data_entity.timestamps)))
                 fb_data_entity.linkmldto.date_updated_curie = strict_rfc3339.\
