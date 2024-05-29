e2a1320491c64e5ff77dec7d15e8dcba0be42f62
gildossantos <gilbertodossantos74@gmail.com>
2024-05-29 11:10:25 -0400

    guess at missing synonym fix

--- src/entity_handler.py
+++ src/entity_handler.py
@@ -552,7 +552,8 @@ class PrimaryEntityHandler(DataHandler):
             'synonym_bin': '_synonym_dtos'
         }
         map_synonyms_required = False
-        linkml_dto_attributes = self.agr_linkmldto_dict[self.fb_data_type].__dict__.keys()
+        # BOB - test simple fix.
+        linkml_dto_attributes = self.agr_linkmldto_dict[self.fb_data_type]().__dict__.keys()
         for dto_key in linkml_dto_attributes:
             for bin_type, bin_suffix in linkml_synonym_slots.items():
                 if dto_key.endswith(bin_suffix):
