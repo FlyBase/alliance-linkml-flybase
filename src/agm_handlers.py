2c09245b487f26fd5379def4e241763ded7eab71
gildossantos <gilbertodossantos74@gmail.com>
2024-11-05 08:23:26 -0500

    revert test

--- src/agm_handlers.py
+++ src/agm_handlers.py
@@ -71,7 +71,10 @@ class StrainHandler(PrimaryEntityHandler):
         """Map basic FlyBase strain data to the Alliance object."""
         self.log.info('Map basic strain info.')
         for strain in self.fb_data_entities.values():
-            agr_strain = self.agr_export_type()
+            self.log.info('Using v0.')
+            agr_strain = agr_export_type()
+            # self.log.info('Using v1.').
+            # agr_strain = self.agr_export_type()
             agr_strain.obsolete = strain.chado_obj.is_obsolete
             agr_strain.mod_entity_id = f'FB:{strain.uniquename}'
             agr_strain.mod_internal_id = str(strain.chado_obj.strain_id)
