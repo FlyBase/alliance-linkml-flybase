6707e7bb382443e6c0525bf6ad2ef59382b9a86a
gildossantos <gilbertodossantos74@gmail.com>
2024-12-16 11:24:40 -0500

    add HGNC to db prefix dict

--- src/handler.py
+++ src/handler.py
@@ -92,7 +92,8 @@ class DataHandler(object):
         'Xenbase': 'Xenbase',
         'RGD': 'RGD',
         'MGD': 'MGI',
-        'MGI': 'MGI'
+        'MGI': 'MGI',
+        'HGNC': 'HGNC',
     }
 
     # Specify page_area for cross-references for specific external databases.
