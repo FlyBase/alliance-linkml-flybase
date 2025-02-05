9b8c3aa5ae76e2c8579793179244a9e51c15c413
gildossantos <gilbertodossantos74@gmail.com>
2025-02-05 11:39:37 -0500

    FTA-30: script to get or create genotype from chado

---
+++ src/retrieve_genotypes.py
@@ -0,0 +1,324 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+r"""Retrieve (get or create) a genotype for curation.
+
+Author(s):
+    Gil dos Santos dossantos@morgan.harvard.edu
+
+Usage:
+    retrieve_genotypes.py [-h] [-v VERBOSE] [-t TESTING] [-c CONFIG]
+    [-p PUB] [-i GENOTYPE_INPUT] [-f GENOTYPES_FILE]
+
+Example:
+    python retrieve_genotypes.py -v -t
+    -c /foo/bar/config.cfg
+    -p FBrf0123456
+    -i "&agr;Tub84D[1]/Df(2L)x Scer\GAL4[wg-Gal4] &agr;Tub84D[UAS.HA]"
+    -f ./genotypes_to_make.txt
+
+Notes:
+    Given a genotype name (a string of feature SGML symbols) in the command
+    line (enclosed in double quotes), or, a file of genotype names (one per
+    line), this script will get the existing genotype from chado, or, create a
+    new genotype in chado. Allowed features: alleles, aberrations, balancers,
+    and internal "bogus symbols" (e.g., wg[+]). Insertions (FBti) and
+    constructs (FBtp) are allowed but discouraged. No genotype is created if
+    any quality checks fail.
+    The script returns genotype IDs and descriptions for all input genotypes.
+    Writing to chado entails writing to genotype and feature_genotype tables,
+    as well as assigning an FBgo ID (genotype_dbxref), and a current synonym
+    (genotype_synonym). For each new genotype, a unique string for the
+    genotype is built from the IDs of its component features. This is saved in
+    the genotype.description. Note that feature_genotype cgroup and rank
+    values reflect the alphabetical sorting of the genotype component SGML
+    symbols.
+    This script is designed to be used by curators during curation of genotype
+    associated data (starting with disease ontology annotations).
+    FUTURE: when a new genotype is created in chado, it is then immediately
+    created at the Alliance as well (in the persistent store), to support
+    curation using their interface.
+
+"""
+
+import argparse
+# import datetime
+import sys
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
+from harvdev_utils.production import (
+    Feature, FeaturePub, Pub
+)
+from harvdev_utils.genotype_utilities import GenotypeAnnotation
+from harvdev_utils.psycopg_functions import set_up_db_reading
+
+# Important label for output files.
+report_label = 'genotypes_retrieved'
+
+# Now proceed with generic setup.
+set_up_dict = set_up_db_reading(report_label)
+server = set_up_dict['server']
+database = set_up_dict['database']
+username = set_up_dict['username']
+password = set_up_dict['password']
+output_dir = set_up_dict['output_dir']
+genotype_report_filename = f'{output_dir}{report_label}.report'
+log = set_up_dict['log']
+TESTING = set_up_dict['testing']
+
+# Create SQL Alchemy engines from environmental variables.
+engine_var_rep = 'postgresql://' + username + ":" + password + '@' + server + '/' + database
+ENGINE = create_engine(engine_var_rep)
+insp = inspect(ENGINE)
+
+# Process additional input parameters not handled by the set_up_db_reading() function above.
+parser = argparse.ArgumentParser(description='inputs')
+run_mode = parser.add_mutually_exclusive_group(required=True)
+run_mode.add_argument('-i', '--genotype_input', help='The genotype name to get or create.', required=False)
+run_mode.add_argument('-f', '--genotypes_file', help='A file of genotype names to get or create.', required=False)
+parser.add_argument('-p', '--pub_id', help='The FBrf ID for the publication.', required=True)
+
+# Use parse_known_args(), not parse_args(), to handle args specific to this script (outside of set_up_db_reading()).
+try:
+    args, extra_args = parser.parse_known_args()
+    log.info(f'Parsing args specific to this script: {args}.')
+    GENOTYPE_INPUT = args.genotype_input
+    GENOTYPE_FILE = args.genotypes_file
+    FBRF_PUB_ID = args.pub_id
+except SystemExit as e:
+    log.error('ERROR: Must supply two arguments: -p/--pub (FBrf ID), and one of -i/--genotype_input or -f/--genotypes_file.')
+    sys.exit(e.code)
+
+
+# The main process.
+def main():
+    """Run the steps for checking and updating genotypes."""
+    global GENOTYPE_INPUT
+    global GENOTYPE_FILE
+    global PUB_ID
+    log.info('Running script "{}"'.format(__file__))
+    log.info('STARTED MAIN FUNCTION.\n')
+    if GENOTYPE_INPUT:
+        genotype_input_list = [GENOTYPE_INPUT]
+    else:
+        genotype_file_contents = open(GENOTYPE_FILE, 'r')
+        genotype_input_list = [i.strip() for i in genotype_file_contents if i.strip() != '']
+    genotype_handler_instance = GenotypeHandler(genotype_input_list, FBRF_PUB_ID)
+    db_transaction(genotype_handler_instance)
+    log.info('ENDED MAIN FUNCTION.\n')
+
+
+class GenotypeHandler(object):
+    """This object processes genotype name inputs and gets or creates chado genotypes."""
+    def __init__(self, genotype_input_list, fbrf_pub_id):
+        """Create the GenotypeHandler object.
+
+        Args:
+            genotype_input_list (list): A list of genotype names (allele SGML symbols).
+            pub_id (str): The FBrf ID for the genotype.
+
+        Returns:
+            A GenotypeHandler object.
+
+        """
+        self.genotype_input_list = genotype_input_list
+        self.fbrf_pub_id = fbrf_pub_id
+        self.pub_id = None
+        self.pub_associated_feature_ids = []    # List of current and public features having FB IDs associated with this pub.
+        self.genotype_annotations = []          # List of GenotypeAnnotation objects generated from the input list.
+        self.uname_genotype_annotations = {}    # uniquename-keyed GenotypeAnnotations (for grouping redundant entries).
+
+    def get_pub_id(self, session):
+        """Get pub.pub_id for given FBrf ID."""
+        log.info(f'Look up chado pub for "{self.fbrf_pub_id}".')
+        filters = (
+            Pub.is_obsolete.is_(False),
+            Pub.uniquename.op('~')(r'^FBrf[0-9]{7}$'),
+            Pub.uniquename == self.fbrf_pub_id,
+        )
+        try:
+            result = session.query(Pub).filter(*filters).one()
+            self.pub_id = result.pub_id
+            log.info(f'Found ONE current pub in chado for "{self.fbrf_pub_id}".')
+        except NoResultFound as ne:
+            log.error(f'Found ZERO current pubs in chado for "{self.fbrf_pub_id}": {ne}.')
+            raise NoResultFound
+        except MultipleResultsFound as me:
+            log.error(f'Found MANY current pubs in chado for "{self.fbrf_pub_id}": {me}.')
+            raise MultipleResultsFound
+        return
+
+    def get_pub_associated_features(self, session):
+        """Get features associated with the input pub."""
+        log.info('Get features associated with the input pub.')
+        filters = (
+            FeaturePub.pub_id == self.pub_id,
+            Feature.is_obsolete.is_(False),
+            Feature.is_analysis.is_(False),
+        )
+        results = session.query(Feature).\
+            select_from(Feature).\
+            join(FeaturePub, (FeaturePub.feature_id == Feature.feature_id)).\
+            filter(*filters).\
+            distinct()
+        for result in results:
+            self.pub_associated_feature_ids.append(result.feature_id)
+            # log.debug(f'Found this feature_id={result.feature_id}, {result.name} ({result.uniquename})')
+        log.info(f'Found {len(self.pub_associated_feature_ids)} current public features associated with "{self.fbrf_pub_id}".')
+        return
+
+    def parse_genotype_input_list(self, session):
+        """Parse input genotype names into GenotypeAnnotation objects."""
+        log.info('Parse input genotype names into GenotypeAnnotation objects.\n\n\n')
+        log.info(f'Have {len(self.genotype_input_list)} genotypes to process.')
+        for name in self.genotype_input_list:
+            geno_anno = GenotypeAnnotation(name, log)
+            geno_anno.process_genotype_annotation(session)
+            self.genotype_annotations.append(geno_anno)
+        return
+
+    def check_pub_associations(self):
+        """Check that genotype features are already associated with the input pub FBrf ID."""
+        log.info('Check that genotype features are already associated with the input pub FBrf ID.')
+        no_counter = 0
+        for geno_anno in self.genotype_annotations:
+            geno_specific_features = geno_anno.features.keys() - set(self.pub_associated_feature_ids)
+            for feature_id in geno_specific_features:
+                input_symbol = geno_anno.features[feature_id]['input_symbol']
+                # geno_anno.warnings.append(f'"{input_symbol}" is not associated with {self.fbrf_pub_id}')
+                geno_anno.errors.append(f'"{input_symbol}" is not associated with {self.fbrf_pub_id}')
+                log.error(f'"{input_symbol}" is not associated with {self.fbrf_pub_id}')
+                no_counter += 1
+        if no_counter > 0:
+            log.error(f'Found {no_counter} listed features NOT associated with {self.fbrf_pub_id}')
+        return
+
+    def find_redundant_genotype_entries(self):
+        """Find redundant genotype name inputs."""
+        log.info('Find redundant genotype name inputs.')
+        for geno_anno in self.genotype_annotations:
+            if geno_anno.errors:
+                continue
+            if geno_anno.uniquename in self.uname_genotype_annotations.keys():
+                modified_geno_uname = geno_anno.uniquename.replace('<up>', '[').replace('</up>', ']')
+                geno_anno.errors.append(f'{geno_anno} is redundant with another input genotype, sharing this uniquename: {modified_geno_uname}')
+                log.error(f'{geno_anno} is redundant with another input genotype, sharing this uniquename: {geno_anno.uniquename}')
+            else:
+                self.uname_genotype_annotations[geno_anno.uniquename] = geno_anno
+        return
+
+    def report_errors(self):
+        """Log genotype errors."""
+        log.info('Reporting all input genotypes having errors.')
+        for geno_anno in self.genotype_annotations:
+            if geno_anno.errors:
+                log.error(f'STOP processing "{geno_anno.input_genotype_name}" due to these errors: {";".join(geno_anno.errors)}.')
+        return
+
+    def get_or_create_genotypes(self, session):
+        """Find genotypes that already exist in chado, or make them if new."""
+        log.info('Find genotypes that already exist in chado, or make them if new.')
+        known_counter = 0
+        new_counter = 0
+        unclassified_counter = 0
+        newly_created_counter = 0
+        for geno_anno in self.uname_genotype_annotations.values():
+            geno_anno.get_known_or_create_new_genotype(session)
+            if geno_anno.is_new is True:
+                new_counter += 1
+                if geno_anno.curie is not None:
+                    newly_created_counter += 1
+            elif geno_anno.is_new is False:
+                known_counter += 1
+            else:
+                unclassified_counter += 1
+        input_count = len(self.genotype_annotations)
+        error_count = input_count - len(self.uname_genotype_annotations.keys())
+        log.info(f'Had {input_count} input genotypes.')
+        log.info(f'Found {known_counter} known genotypes.')
+        log.info(f'Found {new_counter} new genotypes: created {newly_created_counter} new genotypes in chado.')
+        log.info(f'Ignored {error_count} genotypes having errors detected.')
+        if unclassified_counter > 0:
+            log.error(f'Found {unclassified_counter} UNCLASSIFIED genotypes.')
+        return
+
+    def print_curator_genotype_report(self):
+        """Print out genotype report."""
+        log.info('Print out genotype report.')
+        report = open(genotype_report_filename, 'w')
+        # now = datetime.datetime.now().strftime("%a %b %d %Y at %H:%M:%S")
+        lines_to_write = []
+        for geno_anno in self.genotype_annotations:
+            lines_to_write.append(f'INPUT: {geno_anno.input_genotype_name}')
+            if geno_anno.curie:
+                lines_to_write.append(f'\tCURIE: FB:{geno_anno.curie}')
+            else:
+                lines_to_write.append('\tCURIE:')
+            if geno_anno.errors:
+                status = 'ERRORS FOUND'
+            elif geno_anno.is_new is True:
+                status = 'NEW GENOTYPE CREATED'
+            elif geno_anno.is_new is False:
+                status = 'KNOWN CHADO GENOTYPE'
+            lines_to_write.append(f'\tSTATUS: {status}')
+            if geno_anno.uniquename:
+                uniquename_output = geno_anno.uniquename.replace('<up>', '[').replace('</up>', ']')
+            else:
+                uniquename_output = ''
+            lines_to_write.append(f'\tUNIQUENAME: {uniquename_output}')
+            lines_to_write.append(f'\tDESCRIPTION: {geno_anno.description}')
+            for error in geno_anno.errors:
+                lines_to_write.append(f'\tERROR: {error}')
+            lines_to_write.append('')
+        for line in lines_to_write:
+            report.write(f'{line}\n')
+        return
+
+    def run(self, session):
+        """Run all GenotypeHandler methods in the correct order."""
+        self.get_pub_id(session)
+        self.get_pub_associated_features(session)
+        self.parse_genotype_input_list(session)
+        self.check_pub_associations()
+        self.find_redundant_genotype_entries()
+        self.report_errors()
+        self.get_or_create_genotypes(session)
+        self.print_curator_genotype_report()
+        return
+
+
+def db_transaction(object_to_execute):
+    """Interact with the chado database given an object that has a "run()" method.
+
+    Args:
+        object_to_execute: Some object that has an SQL ORM "run()" method.
+
+    Returns:
+        An sqlalchemy session that can be used later on for updating the queried objects.
+
+    Raises:
+        Raises a RuntimeError if there are problems with executing the query.
+
+    """
+    global ENGINE
+    global TESTING
+    Session = sessionmaker(bind=ENGINE)
+    session = Session()
+    try:
+        object_to_execute.run(session)
+        session.flush()
+    except RuntimeError:
+        session.rollback()
+        log.critical('Critical transaction error occurred during chado query; rolling back and exiting.')
+        raise
+    if TESTING is True:
+        log.info('Since "testing" is True, rolling back all transactions.')
+        session.rollback()
+    else:
+        log.info('Since "testing" is False, committing transactions.')
+        session.commit()
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/run_retrieve_genotypes.py
@@ -0,0 +1,106 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+r"""Run the "retrieve_genotypes.py" script in docker.
+
+Author(s):
+    Gil dos Santos dossantos@morgan.harvard.edu
+
+Usage:
+    run_retrieve_genotypes.py [-h]
+    [-p PUB] [-i GENOTYPE_INPUT] [-f GENOTYPES_FILE]
+
+Example (-i and -f are mutually exclusive):
+    python run_retrieve_genotypes.py
+    -p FBrf0123456
+    -i "&agr;Tub84D[1]/Df(2L)xd Scer\GAL4[wg-Gal4] &agr;Tub84D[UAS.HA]"
+    -f ./genotypes_to_make.txt
+
+Notes:
+    Give this script a single genotype name or a file with a list of names
+    to get FlyBase genotype (FBgo) IDs for them. If the genotype does not
+    yet exist in chado, it will be created on the spot. If the genotype is
+    not yet present at the Alliance, it will be exported there immediately.
+    A genotype name should be a string of feature SGML symbols, as would be
+    given in a proforma record: e.g., &agrTub67C[3]. Two alleles at the same
+    locus should be separated by a "/" character; use spaces only to separate
+    components at different loci. Allowed features: alleles, aberrations,
+    balancers, and internal "bogus symbols" (e.g., wg[+]). Insertions (FBti)
+    and constructs (FBtp) are also allowed, but discouraged.
+    If any quality checks fail, processing stops and the errors are reported.
+
+"""
+
+import argparse
+import configparser
+import os
+import subprocess
+import sys
+
+# Process input parameters.
+parser = argparse.ArgumentParser(
+    description='The "run_retrieve_genotypes.py" script gets IDs for genotypes during curation. It creates them as needed.',
+    epilog="""
+    Tips for specifying a genotype:
+    Enclose the genotype string, a list of allele, aberration or insertion symbols, in double quotes.
+    For alleles, use of "SGML" for Greek chars is preferred: e.g., "&agrTub67C[1]". However, plain text works too: e.g., "alphaTub67C[1]".
+    Also allowed are "bogus symbol" features that represent an internal wild-type allele: e.g., "wg[+]".
+    Alleles sharing a locus should be separated by a "/" character: e.g., "wg[1]/wg[l-17]".
+    An allele and deficiency can share a locus: e.g., "wg[1]/Df(2L)xyz".
+    Use spaces to separate components of different loci: e.g., "wg[1]/wg[+] sona[18]/sona[18]".
+    By convention, transgenic alleles for a gene are listed in their own loci, separate from classical allele loci: e.g., "wg[1]/wg[1] UAS-wg[HA]".
+    """,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+)
+parser.add_argument('-p', '--pub', help='The FBrf ID for the publication.', required=True)
+run_mode = parser.add_mutually_exclusive_group(required=True)
+run_mode.add_argument('-i', '--genotype_input', help='The genotype name to get or create.', required=False)
+run_mode.add_argument('-f', '--genotypes_file', help='A file of genotype names to get or create.', required=False)
+
+try:
+    args = parser.parse_args()
+    genotype_input = args.genotype_input
+    genotype_input_file = args.genotypes_file
+    fbrf_pub_id = args.pub
+except SystemExit as e:
+    print('ERROR: Must supply two arguments: -p/--pub (FBrf ID), and one of -i/--genotype_input or -f/--genotypes_file.')
+    sys.exit(e.code)
+
+# Open config for chado communication.
+config = configparser.ConfigParser()
+config.read('/data/credentials/production/config.cfg')
+server = config['chiacur']['Server']
+database = config['chiacur']['Database']
+user = config['chiacur']['User']
+pg_pwd = config['chiacur']['PGPassword']
+agr_token = config['chiacur']['AllianceCurationAPIToken']
+
+# Construct command for running script in docker.
+command = 'rm -f ./genotypes_retrieved*.report && '
+command += 'rm -f ./genotypes_retrieved*.log && '
+command += 'docker run --user $(id -u):$(id -g) '
+command += '-v ./:/src/input/ '
+command += '-v ./:/src/output/ '
+command += '-v ./:/src/logs/ '
+command += f'--network=\"host\" -e SERVER={server} '
+command += f'-e DATABASE={database} '
+command += f'-e USER={user} '
+command += f'-e PGPASSWORD={pg_pwd} '
+command += '-e RELEASE=production '
+command += '--entrypoint /usr/bin/python3 test_export_to_linkml '    # BOB: change bulk_update docker image for prod
+command += f'bulk-update/src/retrieve_genotypes.py -p {fbrf_pub_id} '
+if genotype_input_file:
+    command += f'-f /src/input/{genotype_input_file} '
+else:
+    command += f'-i \"{genotype_input}\" '
+
+# For debugging.
+# print(command)
+# subprocess.run(["bash", "-c", command])
+
+# Using devnull to suppress confusing matplotlib and bioservices warnings that I can't resolve.
+with open(os.devnull, 'w') as devnull:
+    subprocess.run(["bash", "-c", command], stdout=devnull, stderr=devnull)
+
+report = open('./genotypes_retrieved.report', 'r')
+for line in report:
+    print(line.rstrip())
