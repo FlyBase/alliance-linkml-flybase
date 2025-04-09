efacd0bbc9bff1573c89ec48b6be78dbe1cde4ed
gildossantos <gilbertodossantos74@gmail.com>
2025-04-09 12:01:36 -0400

    raise exception if no db

--- src/run_retrieve_genotypes.py
+++ src/run_retrieve_genotypes.py
@@ -83,7 +83,7 @@ try:
 except psycopg2.OperationalError as e:
     print('An error occurred while trying to connect to the database.')
     print(f'Error message: {e}')
-    raise
+    sys.exit(1)
 
 # Construct command for running script in docker.
 command = 'rm -f ./genotypes_retrieved*.report && '
