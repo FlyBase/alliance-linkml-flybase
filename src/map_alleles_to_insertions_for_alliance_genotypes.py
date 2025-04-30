42d2cc355020cea5c8c55059e7b524fe5ed0deec
gildossantos <gilbertodossantos74@gmail.com>
2025-04-30 10:00:24 -0400

    fix flush

--- src/map_alleles_to_insertions_for_alliance_genotypes.py
+++ src/map_alleles_to_insertions_for_alliance_genotypes.py
@@ -88,14 +88,11 @@ class AlleleMapper(AlleleHandler):
     def initial_flush(self, session):
         """Flush existing "is_represented_at_alliance_as" feature_relationships to create a blank slate."""
         self.log.info('Flush existing "is_represented_at_alliance_as" feature_relationships to create a blank slate.')
-        filters = (
-            Cvterm.name == 'is_represented_at_alliance_as',
-        )
-        session.query(FeatureRelationship).\
-            select_from(FeatureRelationship).\
-            join(Cvterm, (Cvterm.cvterm_id == FeatureRelationship.type_id)).\
-            filter(*filters).\
-            delete()
+        filters = (Cvterm.name == 'is_represented_at_alliance_as', )
+        fr_type_cvterm_id = session.query(Cvterm).filter(*filters).one().cvterm_id
+        self.log.info(f'The "is_represented_at_alliance_as" CV term corresponds to cvterm.cvterm_id={fr_type_cvterm_id}')
+        session.query(FeatureRelationship).filter(FeatureRelationship.type_id == fr_type_cvterm_id).delete()
+
         # results = session.query(FeatureRelationship).\
         #     select_from(FeatureRelationship).\
         #     join(Cvterm, (Cvterm.cvterm_id == FeatureRelationship.type_id)).\
@@ -104,7 +101,8 @@ class AlleleMapper(AlleleHandler):
         # for result in results:
         #     session.query(FeatureRelationship).filter(FeatureRelationship.feature_relationship_id == result.feature_relationship_id).delete()
         #     counter += 1
-        # self.log.info(f'Flushed {counter} "is_represented_at_alliance_as" feature_relationships before updating.')
+        self.log.info('Flushed all "is_represented_at_alliance_as" feature_relationships before updating.')
+        quit()
         return
 
     # Add methods to be run by get_general_data() below.
