07869b0d908d2e99ec5a8302e7548fc4ab5ac4e8
gildossantos <gilbertodossantos74@gmail.com>
2025-07-10 13:01:55 -0400

    debug

--- src/retrieve_genotypes.py
+++ src/retrieve_genotypes.py
@@ -74,6 +74,9 @@ TESTING = set_up_dict['testing']
 AGR_TOKEN = os.environ['ALLIANCETOKEN']
 if not AGR_TOKEN:
     raise ValueError("ALLIANCETOKEN environment variable is required")
+# BOB
+else:
+    log.debug(f'Have this: {AGR_TOKEN}')
 AGR_BASE_URL = os.environ.get('AGR_BASE_URL', 'https://beta-curation.alliancegenome.org')    # BOB - POINT TO REAL PROD ONCE TESTING IS COMPLETE
 
 # Create SQL Alchemy engines from environmental variables.
