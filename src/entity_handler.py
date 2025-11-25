3780e58bc583b731f492ad80fdb7d32943c834e7
ianlongden <ianlongden@morgan.harvard.edu>
2025-11-25 10:33:58 -0500

    debug

--- src/entity_handler.py
+++ src/entity_handler.py
@@ -803,8 +803,8 @@ class PrimaryEntityHandler(DataHandler):
         fkey_col = self.get_foreign_key_column(asso_chado_table, main_pkey_name)
         filters = (
             fkey_col.in_((self.fb_data_entities.keys())),
-            asso_chado_table.is_current.is_(True),
-            Db.name.in_((self.fb_agr_db_dict.keys()))
+            asso_chado_table.is_current.is_(True)
+            # Db.name.in_((self.fb_agr_db_dict.keys()))
         )
         results = session.query(asso_chado_table).\
             join(Dbxref, (Dbxref.dbxref_id == asso_chado_table.dbxref_id)).\
