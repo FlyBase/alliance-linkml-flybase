136ea9473d13412586a31068e6009f7887b684e0
ianlongden <ianlongden@morgan.harvard.edu>
2025-12-12 14:20:43 -0500

    debug associations

--- src/cassette_handler.py
+++ src/cassette_handler.py
@@ -270,7 +270,8 @@ class CassetteHandler(FeatureHandler):
             self.log.debug(f"BOB: cas cass rel: {cassette_cassette_rels}")
             self.log.debug(f"BOB: chado_obj: {cassette_cassette_rels[0].chado_obj}")
             self.log.debug(f"BOB: chado_obj: {dir(cassette_cassette_rels[0].chado_obj)}")
-            self.log.debug(f"BOB: chado_obj: {cassette_cassette_rels[0].chado_obj.dbxref}")
+            self.log.debug(f"BOB: chado_obj: {cassette_cassette_rels[0].chado_obj.type}")
+            self.log.debug(f"BOB: chado_obj: {dir(cassette_cassette_rels[0].chado_obj.type)}")
 
             # self.log.debug(f"BOB: cassette_cassette_rels[0]: {cassette_cassette_rels[0]}")
             rel_dto = agr_datatypes.CassetteAssociationDTO(
