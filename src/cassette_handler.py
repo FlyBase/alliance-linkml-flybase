dd1bfd3920e1325bfde80907641c81972c897171
ianlongden <ianlongden@morgan.harvard.edu>
2025-12-12 14:24:10 -0500

    debug associations

--- src/cassette_handler.py
+++ src/cassette_handler.py
@@ -265,14 +265,14 @@ class CassetteHandler(FeatureHandler):
             pub_curies = self.lookup_pub_curies(all_pub_ids)
 
             # Adjust allele-gene relation_type as needed.
-            rel_type_name = 'compatible_tool'
 
             self.log.debug(f"BOB: cas cass rel: {cassette_cassette_rels}")
             self.log.debug(f"BOB: chado_obj: {cassette_cassette_rels[0].chado_obj}")
             self.log.debug(f"BOB: chado_obj: {dir(cassette_cassette_rels[0].chado_obj)}")
             self.log.debug(f"BOB: chado_obj: {cassette_cassette_rels[0].chado_obj.type}")
             self.log.debug(f"BOB: chado_obj: {dir(cassette_cassette_rels[0].chado_obj.type)}")
-
+            # rel_type_name = 'compatible_tool'
+            rel_type_name = cassette_cassette_rels[0].chado_obj.type.name
             # self.log.debug(f"BOB: cassette_cassette_rels[0]: {cassette_cassette_rels[0]}")
             rel_dto = agr_datatypes.CassetteAssociationDTO(
                 subject_curie, object_curie,
