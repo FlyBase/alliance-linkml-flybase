2a7ad6a12f19b43da04b6a5c55572d2eec178b2d
ianlongden <ianlongden@morgan.harvard.edu>
2025-12-16 11:42:04 -0500

    fix transgenic tool assoc

--- src/AGR_data_retrieval_curation_transgenic_tool.py
+++ src/AGR_data_retrieval_curation_transgenic_tool.py
@@ -110,7 +110,7 @@ def generate_association_tsv_file(export_dict, filename):
     filename = filename.replace('.tsv', '_associations.tsv')
     with open(filename, 'w') as outfile:
         outfile.write("# Object curie\tSubject curie\tPub\n")
-        for entity_dict in export_dict['tool_association_ingest_set']:
+        for entity_dict in export_dict['transgenic_tool_transgenic_tool_association_ingest_set']:
             obj = entity_dict['transgenic_tool_transgenic_tool_association_object']
             sub = entity_dict['transgenic_tool_association_subject']
             pubs = "|".join(entity_dict['evidence'])
