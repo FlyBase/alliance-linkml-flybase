dea7c41ef9383b2535541d83e7b173317a7f26b9
ianlongden <ianlongden@morgan.harvard.edu>
2026-02-03 15:24:33 -0500

    debug

--- src/entity_handler.py
+++ src/entity_handler.py
@@ -583,8 +583,9 @@ class PrimaryEntityHandler(DataHandler):
                 print(f"\tBOB: cvtermprop sub id:{entity_cvterm_id} name:{entity_prop_type_name}")
                 print(dir(cvtermprop_result.feature_cvterm))
                 print(dir(cvtermprop_result))
-                print(f"\tBOB: sub sub {cvtermprop_result.feature_cvterm.cvterm}")
-                print(f"\tBOB: sub sub sub {cvtermprop_result.feature_cvterm.cvterm.dbxref}")
+                print(F"BOBBY: {entity_prop_type_name} {cvtermprop_result.feature_cvterm.cvterm.name} {cvtermprop_result.feature_cvterm.cvterm.dbxref.name}")
+                print(f"\tBOB: sub sub {cvtermprop_result.feature_cvterm.cvterm.name}")
+                print(f"\tBOB: sub sub sub {cvtermprop_result.feature_cvterm.cvterm.dbxref.name}")
                 print(f"\tBOB: feature_cvterm.type {cvtermprop_result.type}")
             if entity_prop_type_name in cvterm_annotation_dict[entity_cvterm_id].props_by_type.keys():
                 cvterm_annotation_dict[entity_cvterm_id].props_by_type[entity_prop_type_name].append(fb_datatypes.FBProp(cvtermprop_result))
