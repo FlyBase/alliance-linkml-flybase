a96ba8cad63a4afeec2b03daa00681f97061e1e7
ianlongden <ianlongden@morgan.harvard.edu>
2026-02-27 14:02:59 -0500

    Debug

--- src/cassette_handler.py
+++ src/cassette_handler.py
@@ -532,19 +532,19 @@ class CassetteHandler(FeatureHandler):
                 if self.testing:
                     cassette = self.fb_data_entities[cassette_rel.chado_obj.subject_id]
                     component = self.feature_lookup[cassette_rel.chado_obj.object_id]
-                    self.log.debug(f"BOB {cassette.uniquename} {component.uniquename} {cassette_rel.chado_obj.type.name}")
+                    self.log.debug(f"BOB {cassette['uniquename']} {component['uniquename']} {cassette_rel.chado_obj.type.name}")
                 try:
                     self.cassette_cassette_rels[cassette_cassette_key].append(cassette_rel)
                     if self.testing:
                         cassette = self.fb_data_entities[cassette_rel.chado_obj.subject_id]
                         component = self.feature_lookup[cassette_rel.chado_obj.object_id]
-                        self.log.debug(f"BOB {cassette.uniquename} {component.uniquename} {cassette_rel.chado_obj.type.name} APPENDED")
+                        self.log.debug(f"BOB {cassette['uniquename']} {component['uniquename']} {cassette_rel.chado_obj.type.name} APPENDED")
                 except KeyError:
                     self.cassette_cassette_rels[cassette_cassette_key] = [cassette_rel]
                     if self.testing:
                         cassette = self.fb_data_entities[cassette_rel.chado_obj.subject_id]
                         component = self.feature_lookup[cassette_rel.chado_obj.object_id]
-                        self.log.debug(f"BOB {cassette.uniquename} {component.uniquename} {cassette_rel.chado_obj.type.name} NEW")
+                        self.log.debug(f"BOB {cassette['uniquename']} {component['uniquename']} {cassette_rel.chado_obj.type.name} NEW")
 
                     component_counter += 1
         self.log.info(f'Found {component_counter} components for {cassette_counter} cassettes.')
