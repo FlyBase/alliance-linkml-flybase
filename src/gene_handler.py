396df80cf50f6b62ffded96d57aff05f0b2aec0d
ianlongden <ianlongden@morgan.harvard.edu>
2026-03-05 14:43:42 -0500

    add gene snapshot text

--- src/gene_handler.py
+++ src/gene_handler.py
@@ -246,7 +246,7 @@ class GeneHandler(FeatureHandler):
         # self.map_pubs()    # Suppress until LinkML Gene gets reference_curies slot.
         self.map_timestamps()
         self.map_secondary_ids('gene_secondary_id_dtos')
-        # self.map_gene_snapshot()
+        self.map_gene_snapshot()
         self.map_gene_type()
         self.map_gene_panther_xrefs()
         self.map_anno_ids_to_secondary_ids('gene_secondary_id_dtos')
